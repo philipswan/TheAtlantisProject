eefa742a68e50481c84bd753ae04bc5665701e81
philipswan <phil.swan@hotmail.com>
2017-10-30 22:02:42 -0700

    Just tweaked the size of the habitats and the final scale a bit

--- Assets/OVR/Scripts/OVRLint.cs
+++ Assets/OVR/Scripts/OVRLint.cs
@@ -11,7 +11,7 @@ You may obtain a copy of the License at
 
 http://www.oculus.com/licenses/LICENSE-3.3
 
-Unless required by applicable law or agreed to in writing, the Oculus VR SDK
+Unless required by applihab law or agreed to in writing, the Oculus VR SDK
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
