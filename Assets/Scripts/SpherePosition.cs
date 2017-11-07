ac45aa2659013960503d39df2e284042c17da016
philipswan <phil.swan@hotmail.com>
2017-11-06 21:31:50 -0800

    Added numerous files from hack-a-thon

---
+++ Assets/Scripts/SpherePosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePosition : MonoBehaviour {
+
+    public float RingLatitude = -40;
+    private float EarthRadius = 1;
+    private float RingAltitude = 32f / 6371f;
+    private Vector3 EarthPosition;
+
+	// Use this for initialization
+	void Start () {
+        //EarthPosition.x = -Mathf.Cos(RingLatitude * Mathf.PI / 180) / 2 * (EarthRadius + RingAltitude) / EarthRadius;
+        //EarthPosition.y = -Mathf.Sin(RingLatitude * Mathf.PI / 180) / 2 * (EarthRadius + RingAltitude) / EarthRadius;
+        //y = 0.4478496f;
+        //EarthPosition.z = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        //transform.localPosition = EarthPosition;
+        //transform.Rotate(0, -1.3f * Time.deltaTime, 0);
+        transform.Rotate(0, -.005f * Time.deltaTime, 0);
+    }
+}
