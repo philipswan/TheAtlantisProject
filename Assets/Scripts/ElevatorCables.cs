c950df27e263ab5922c79e9c3847b323b0f5f6a6
Neil Orgon <neil.orgon@gmail.com>
2017-11-08 17:57:09 -0800

    Move the camera at the same rate as the elevator

--- Assets/Scripts/ElevatorCables.cs
+++ Assets/Scripts/ElevatorCables.cs
@@ -11,195 +11,202 @@ using UnityEngine.SceneManagement;
 public class ElevatorCables : MonoBehaviour
 {
 
-    public GameObject carrier_prefab;
-    public GameObject climber_prefab;
-    //public GameObject top_terminal_prefab;
-    public Transform ring_edge_transform;
-    public float ringLatitude = -40;
-    public float tubeRadius = 0.00001f;
-    public int numTubeSides = 8;
-    public int numElevatorCables = 90;
-    public float ringAltitude = .0003f;
-    //public Waypoints Keymanager;
-    private float tetheredRingRadius;
+	public GameObject carrier_prefab;
+	public GameObject climber_prefab;
+	//public GameObject top_terminal_prefab;
+	public Transform ring_edge_transform;
+	public float ringLatitude = -40;
+	public float tubeRadius = 0.00001f;
+	public int numTubeSides = 8;
+	public int numElevatorCables = 90;
+	public float ringAltitude = .0003f;
+	//public Waypoints Keymanager;
+	private float tetheredRingRadius;
 
-    void Start()
-    {
-        tetheredRingRadius = Mathf.Cos(ringLatitude * Mathf.PI / 180) / 2;
-        RefreshElevatorCables();
-    }
+	void Start()
+	{
+		tetheredRingRadius = Mathf.Cos(ringLatitude * Mathf.PI / 180) / 2;
+		RefreshElevatorCables();
+	}
 
-    public void DrawCylinder(Vector3 start, Vector3 end, float Radius, Vector3[] vertices, int[] triangleIndices, int tubePrimitiveBaseOffset, int tubeIndexBaseOffset)
-    {
-        Vector3 v1 = (end - start).normalized;
-        Vector3 v2 = Vector3.Cross(v1, Vector3.up).normalized * Radius;
-        Vector3 v3 = Vector3.Cross(v1, v2).normalized * Radius;
-        float r;
+	public void DrawCylinder(Vector3 start, Vector3 end, float Radius, Vector3[] vertices, int[] triangleIndices, int tubePrimitiveBaseOffset, int tubeIndexBaseOffset)
+	{
+		Vector3 v1 = (end - start).normalized;
+		Vector3 v2 = Vector3.Cross(v1, Vector3.up).normalized * Radius;
+		Vector3 v3 = Vector3.Cross(v1, v2).normalized * Radius;
+		float r;
 
-        //Debug.DrawRay(transform.TransformPoint(start), transform.TransformDirection(v2 * 1000), Color.red, 100);
-        //Debug.DrawRay(transform.TransformPoint(start), transform.TransformDirection(v3 * 1000), Color.green, 100);
-        //Debug.DrawRay(transform.TransformPoint(end), transform.TransformDirection(v2 * 1000), Color.yellow, 100);
-        //Debug.DrawRay(transform.TransformPoint(end), transform.TransformDirection(v3 * 1000), Color.blue, 100);
+		//Debug.DrawRay(transform.TransformPoint(start), transform.TransformDirection(v2 * 1000), Color.red, 100);
+		//Debug.DrawRay(transform.TransformPoint(start), transform.TransformDirection(v3 * 1000), Color.green, 100);
+		//Debug.DrawRay(transform.TransformPoint(end), transform.TransformDirection(v2 * 1000), Color.yellow, 100);
+		//Debug.DrawRay(transform.TransformPoint(end), transform.TransformDirection(v3 * 1000), Color.blue, 100);
 
-        for (int j = 0; j < numTubeSides; j++)
-        {
-            // Find next (or first) vertex offset
-            int m = (j + 1) % numTubeSides; // changed currentTube.Count to numTubeSides
+		for (int j = 0; j < numTubeSides; j++)
+		{
+			// Find next (or first) vertex offset
+			int m = (j + 1) % numTubeSides; // changed currentTube.Count to numTubeSides
 
-            // Find the 4 vertices that make up a quad
-            int iv0 = tubePrimitiveBaseOffset + j * 2 + 0;
-            int iv1 = tubePrimitiveBaseOffset + j * 2 + 1;
-            int iv2 = tubePrimitiveBaseOffset + m * 2 + 0;
-            int iv3 = tubePrimitiveBaseOffset + m * 2 + 1;
+			// Find the 4 vertices that make up a quad
+			int iv0 = tubePrimitiveBaseOffset + j * 2 + 0;
+			int iv1 = tubePrimitiveBaseOffset + j * 2 + 1;
+			int iv2 = tubePrimitiveBaseOffset + m * 2 + 0;
+			int iv3 = tubePrimitiveBaseOffset + m * 2 + 1;
 
-            r = j * 2.0f * Mathf.PI / numTubeSides;
-            Vector3 v4 = Mathf.Cos(r) * v2 + Mathf.Sin(r) * v3;
-            Vector3 v5 = start + v4;
-            Vector3 v6 = end + v4;
+			r = j * 2.0f * Mathf.PI / numTubeSides;
+			Vector3 v4 = Mathf.Cos(r) * v2 + Mathf.Sin(r) * v3;
+			Vector3 v5 = start + v4;
+			Vector3 v6 = end + v4;
 
-            vertices[iv0] = v5;
-            vertices[iv1] = v6;
+			vertices[iv0] = v5;
+			vertices[iv1] = v6;
 
-            // As we itterate around the circumference of the tube, "Draw" the two triangles that make each tube face
-            int ti = tubeIndexBaseOffset + j * 2 * 3;
-            // Triangle 0
-            triangleIndices[ti + 0] = iv0;
-            triangleIndices[ti + 1] = iv2;
-            triangleIndices[ti + 2] = iv1;
-            // Triangle 1
-            triangleIndices[ti + 3] = iv1;
-            triangleIndices[ti + 4] = iv2;
-            triangleIndices[ti + 5] = iv3;
-        }
-    }
+			// As we itterate around the circumference of the tube, "Draw" the two triangles that make each tube face
+			int ti = tubeIndexBaseOffset + j * 2 * 3;
+			// Triangle 0
+			triangleIndices[ti + 0] = iv0;
+			triangleIndices[ti + 1] = iv2;
+			triangleIndices[ti + 2] = iv1;
+			// Triangle 1
+			triangleIndices[ti + 3] = iv1;
+			triangleIndices[ti + 4] = iv2;
+			triangleIndices[ti + 5] = iv3;
+		}
+	}
 
-    public void RefreshElevatorCables()
-    {
-        // Total vertices - We'll construct a tube for each branch and that tube will have numTubeSides. 
-        int totalVertices = numElevatorCables * numTubeSides * 2;
+	public void RefreshElevatorCables()
+	{
+		// Total vertices - We'll construct a tube for each branch and that tube will have numTubeSides. 
+		int totalVertices = numElevatorCables * numTubeSides * 2;
 
-        // Total primitives
-        int totalPrimitives = numElevatorCables * numTubeSides * 2;
+		// Total primitives
+		int totalPrimitives = numElevatorCables * numTubeSides * 2;
 
-        // Total indices
-        int totalIndices = totalPrimitives * 3;
+		// Total indices
+		int totalIndices = totalPrimitives * 3;
 
-        // Init the mesh
-        Mesh mesh = new Mesh();
+		// Init the mesh
+		Mesh mesh = new Mesh();
 
-        // Init the vertex and triangle arrays
-        Vector3[] vertices = new Vector3[totalVertices];
-        int[] triangleIndices = new int[totalIndices];
+		// Init the vertex and triangle arrays
+		Vector3[] vertices = new Vector3[totalVertices];
+		int[] triangleIndices = new int[totalIndices];
 
-        // Pre-Calculate the size in radians of TubeSide
-        float elevatorCableSpacing = 2.0f * Mathf.PI / (float)numElevatorCables;
-        float tubeSideSize = 2.0f * Mathf.PI / (float)numTubeSides;
+		// Pre-Calculate the size in radians of TubeSide
+		float elevatorCableSpacing = 2.0f * Mathf.PI / (float)numElevatorCables;
+		float tubeSideSize = 2.0f * Mathf.PI / (float)numTubeSides;
 
-        // Create floats for our xyz coordinates, and angles
-        float x2 = 0, y2 = 0, z2 = 0;
-        float x3 = 0, y3 = 0, z3 = 0;
-        float x4 = 0, y4 = 0, z4 = 0;
-        float theta;
-        float phi0;
-        float phi1;
+		// Create floats for our xyz coordinates, and angles
+		float x2 = 0, y2 = 0, z2 = 0;
+		float x3 = 0, y3 = 0, z3 = 0;
+		float x4 = 0, y4 = 0, z4 = 0;
+		float theta;
+		float phi0;
+		float phi1;
 
-        for (int i = 0; i < numElevatorCables; i++)
-        {
-            theta = i * elevatorCableSpacing;
-            phi0 = tetheredRingRadius;
-            phi1 = tetheredRingRadius - ringAltitude * Mathf.Cos(ringLatitude * Mathf.Deg2Rad);
+		for (int i = 0; i < numElevatorCables; i++)
+		{
+			theta = i * elevatorCableSpacing;
+			phi0 = tetheredRingRadius;
+			phi1 = tetheredRingRadius - ringAltitude * Mathf.Cos(ringLatitude * Mathf.Deg2Rad);
 
-            // Find the current and next segments
-            int tubePrimitiveBaseOffset = i * numTubeSides * 2;
-            int tubeIndexBaseOffset = i * numTubeSides * 6;
+			// Find the current and next segments
+			int tubePrimitiveBaseOffset = i * numTubeSides * 2;
+			int tubeIndexBaseOffset = i * numTubeSides * 6;
 
-            x2 = phi1 * Mathf.Cos(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1) - tetheredRingRadius;
-            z2 = phi1 * Mathf.Sin(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1);
-            y2 = tubeRadius * Mathf.Sin(0 * tubeSideSize);
-            x3 = phi1 * Mathf.Cos(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1) - tetheredRingRadius;
-            z3 = phi1 * Mathf.Sin(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1);
-            y3 = tubeRadius * Mathf.Sin(0 * tubeSideSize) - ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
-            x4 = phi1 * Mathf.Cos(theta + tubeRadius * Mathf.Cos(1 * tubeSideSize) / phi1) - tetheredRingRadius;
-            z4 = phi1 * Mathf.Sin(theta + tubeRadius * Mathf.Cos(1 * tubeSideSize) / phi1);
-            y4 = tubeRadius * Mathf.Sin(1 * tubeSideSize) - ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
+			x2 = phi1 * Mathf.Cos(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1) - tetheredRingRadius;
+			z2 = phi1 * Mathf.Sin(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1);
+			y2 = tubeRadius * Mathf.Sin(0 * tubeSideSize);
+			x3 = phi1 * Mathf.Cos(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1) - tetheredRingRadius;
+			z3 = phi1 * Mathf.Sin(theta + tubeRadius * Mathf.Cos(0 * tubeSideSize) / phi1);
+			y3 = tubeRadius * Mathf.Sin(0 * tubeSideSize) - ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
+			x4 = phi1 * Mathf.Cos(theta + tubeRadius * Mathf.Cos(1 * tubeSideSize) / phi1) - tetheredRingRadius;
+			z4 = phi1 * Mathf.Sin(theta + tubeRadius * Mathf.Cos(1 * tubeSideSize) / phi1);
+			y4 = tubeRadius * Mathf.Sin(1 * tubeSideSize) - ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
 
-            // Find the current and next segments
+			// Find the current and next segments
 
-            Vector3 cabletop, cablebot, cableleft;
-            cabletop.x = phi0 * Mathf.Cos(theta) - tetheredRingRadius;
-            cabletop.z = phi0 * Mathf.Sin(theta);
-            cabletop.y = -Mathf.Pow(tetheredRingRadius - phi0, 1.7f) * 10;
-            cablebot.x = phi1 * Mathf.Cos(theta) - tetheredRingRadius;
-            cablebot.z = phi1 * Mathf.Sin(theta);
-            cablebot.y = -ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
-            cableleft.x = phi0 * Mathf.Cos(theta) - tetheredRingRadius;
-            cableleft.z = phi0 * Mathf.Sin(theta);
-            cableleft.y = -ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
+			Vector3 cabletop, cablebot, cableleft;
+			cabletop.x = phi0 * Mathf.Cos(theta) - tetheredRingRadius;
+			cabletop.z = phi0 * Mathf.Sin(theta);
+			cabletop.y = -Mathf.Pow(tetheredRingRadius - phi0, 1.7f) * 10;
+			cablebot.x = phi1 * Mathf.Cos(theta) - tetheredRingRadius;
+			cablebot.z = phi1 * Mathf.Sin(theta);
+			cablebot.y = -ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
+			cableleft.x = phi0 * Mathf.Cos(theta) - tetheredRingRadius;
+			cableleft.z = phi0 * Mathf.Sin(theta);
+			cableleft.y = -ringAltitude * Mathf.Sin(ringLatitude * Mathf.Deg2Rad);
 
-            DrawCylinder(cabletop, cablebot, tubeRadius, vertices, triangleIndices, tubePrimitiveBaseOffset, tubeIndexBaseOffset);
+			DrawCylinder(cabletop, cablebot, tubeRadius, vertices, triangleIndices, tubePrimitiveBaseOffset, tubeIndexBaseOffset);
 
-            var cabletop_old = new Vector3(x2, y2, z2);
-            var cablebot_old = new Vector3(x3, y3, z3);
-            var cableleft_old = new Vector3(x4, y4, z4);
+			var cabletop_old = new Vector3(x2, y2, z2);
+			var cablebot_old = new Vector3(x3, y3, z3);
+			var cableleft_old = new Vector3(x4, y4, z4);
 
-            //var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //go.transform.parent = this.transform; //ring_edge_transform;
-            //go.transform.localPosition = new Vector3(x2, y2, z2);
-            //go.transform.localScale = Vector3.one * 1e-3f;
+			//var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			//go.transform.parent = this.transform; //ring_edge_transform;
+			//go.transform.localPosition = new Vector3(x2, y2, z2);
+			//go.transform.localScale = Vector3.one * 1e-3f;
 
-            if (i <= 4)
-            {
-                // Add an box to represent the ring terminal at the top of each stay 
-                //var acc_top = Instantiate(top_terminal_prefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform);
-                //acc_top.transform.localPosition = Vector3.Lerp(cablebot, cabletop, 0.95f);
-                //acc_top.transform.localScale = Vector3.one * 4e-5f;
-                ////acc_top.transform.localRotation = new Quaternion(0, -90, 0,  0); //This doesn't work
-                //acc_top.transform.LookAt(this.transform.TransformPoint(cableleft_old), this.transform.TransformVector(cabletop_old - cablebot_old));
+			if (i <= 4)
+			{
+				// Add an box to represent the ring terminal at the top of each stay 
+				//var acc_top = Instantiate(top_terminal_prefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform);
+				//acc_top.transform.localPosition = Vector3.Lerp(cablebot, cabletop, 0.95f);
+				//acc_top.transform.localScale = Vector3.one * 4e-5f;
+				////acc_top.transform.localRotation = new Quaternion(0, -90, 0,  0); //This doesn't work
+				//acc_top.transform.LookAt(this.transform.TransformPoint(cableleft_old), this.transform.TransformVector(cabletop_old - cablebot_old));
 
-                // Add an capsule somewher along the length of each stay 
+				// Add an capsule somewher along the length of each stay 
 				GameObject acc_mid = Instantiate(climber_prefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform);
 				acc_mid.transform.localScale = Vector3.one * 1e-5f;
-                acc_mid.transform.localPosition = Vector3.Lerp(cablebot, cabletop, 0.85f);
+				acc_mid.transform.localPosition = Vector3.Lerp(cablebot, cabletop, 0.85f);
 				acc_mid.transform.LookAt(this.transform.TransformPoint(cableleft), this.transform.TransformVector(cabletop_old - cablebot_old));
+				acc_mid.AddComponent<ElevatorMotion>();
 
-                // Add an aircraft carrier to represent the surface terminal at the bottom of each stay 
+				// Add an aircraft carrier to represent the surface terminal at the bottom of each stay 
 				GameObject acc_bot = Instantiate(carrier_prefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform);
 				acc_bot.transform.localScale = Vector3.one * 3e-6f;
-                acc_bot.transform.localPosition = new Vector3(x3, y3, z3);
-                //acc_bot.transform.localPosition = Vector3.Lerp(cablebot, cabletop, 0.2f); // Why doesn't this work???
+				acc_bot.transform.localPosition = new Vector3(x3, y3, z3);
+				//acc_bot.transform.localPosition = Vector3.Lerp(cablebot, cabletop, 0.2f); // Why doesn't this work???
 				acc_bot.transform.LookAt(this.transform.TransformPoint(cableleft), this.transform.TransformVector(cabletop_old - cablebot_old));
 
 				// Set variables for user's elevator
 				if (i == 3)
 				{
+					GameObject topOfRing = new GameObject("Top of ring");
+					topOfRing.transform.parent = transform;
+					topOfRing.transform.localPosition = cabletop;
+
 					acc_mid.GetComponent<ElevatorMotion>().SetPositions(cabletop, cablebot, false);
 					Transition1.Instance.UpdateKeys(acc_mid.transform);
+					Transition1.Instance.MoveCamera(topOfRing.transform);
+					//Transition1.Instance.MoveCamera(topOfRing.transform.position);
 					continue;
 				}
 
 				acc_mid.GetComponent<ElevatorMotion>().SetPositions(cabletop, cablebot, true);
 
-                //acc.transform.rotation = Quaternion.LookRotation(this.transform.TransformVector(Vector3.Cross(cableleft - cablebot, cabletop - cablebot)), this.transform.TransformVector(cabletop - cablebot));
-                //acc.transform.LookAt(this.transform.TransformVector(Vector3.Cross(cableleft - cablebot, cabletop - cablebot)), this.transform.TransformVector(cabletop - cablebot));
+				//acc.transform.rotation = Quaternion.LookRotation(this.transform.TransformVector(Vector3.Cross(cableleft - cablebot, cabletop - cablebot)), this.transform.TransformVector(cabletop - cablebot));
+				//acc.transform.LookAt(this.transform.TransformVector(Vector3.Cross(cableleft - cablebot, cabletop - cablebot)), this.transform.TransformVector(cabletop - cablebot));
 
-//                if (i == 0)
-//                {
-//                    // We're going to set a waypoint so that we can navigate to here later using animation scripts
-//                    //Transform temp = Keymanager.GetComponent<Waypoints>().waypoints[1];
-//                    Keymanager.waypoints[1].position = -acc_bot.transform.position;
-//                    Keymanager.waypoints[1].rotation = acc_bot.transform.rotation;
-//                    Keymanager.waypoints[1].localScale = Vector3.one * 10.0f;
-//                }
-            }
+				//                if (i == 0)
+				//                {
+				//                    // We're going to set a waypoint so that we can navigate to here later using animation scripts
+				//                    //Transform temp = Keymanager.GetComponent<Waypoints>().waypoints[1];
+				//                    Keymanager.waypoints[1].position = -acc_bot.transform.position;
+				//                    Keymanager.waypoints[1].rotation = acc_bot.transform.rotation;
+				//                    Keymanager.waypoints[1].localScale = Vector3.one * 10.0f;
+				//                }
+			}
 
 
-        }
-        mesh.vertices = vertices;
-        mesh.triangles = triangleIndices;
+		}
+		mesh.vertices = vertices;
+		mesh.triangles = triangleIndices;
 
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        MeshFilter mFilter = GetComponent<MeshFilter>(); // tweaked to Generic
-        mFilter.mesh = mesh;
-    }
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		MeshFilter mFilter = GetComponent<MeshFilter>(); // tweaked to Generic
+		mFilter.mesh = mesh;
+	}
 }
--- Assets/Scripts/ElevatorMotion.cs
+++ Assets/Scripts/ElevatorMotion.cs
@@ -4,87 +4,87 @@ using UnityEngine;
 
 public class ElevatorMotion : MonoBehaviour {
 
-    [Tooltip("Time in seconds the elevator will take to travel from the ring to the ship or vice versa.")]
-    public float TravelTime = 10f;
+	[Tooltip("Time in seconds the elevator will take to travel from the ring to the ship or vice versa.")]
+	public float TravelTime = 10f;
 
 	public bool automatic;				// Should the elevator move automatically
 
-    [HideInInspector]
-    public Vector3 CableTop;            // Position at the top the of the cable that the elevator should travel to (the ring)
+	[HideInInspector]
+	public Vector3 CableTop;            // Position at the top the of the cable that the elevator should travel to (the ring)
 
-    [HideInInspector]
-    public Vector3 CableBotton;         // Position at the bottom of the cable that the elevator should travel to (the ship)
+	[HideInInspector]
+	public Vector3 CableBotton;         // Position at the bottom of the cable that the elevator should travel to (the ship)
 
-    private target currentTarget;       // Enum holding the current target (or destination) of the elevator
-    private Vector3 velocity;           // Velocity of the object when starting movement
-    private Vector3 targetPos;          // Position of the destination of the elevator
-    private bool targetSet;             // Flag set when both targets have been set from ElevatorCables.cs
-    private enum target                 // Enum of target types
-    {
-        Top,
-        Bottom
-    };
+	private target currentTarget;       // Enum holding the current target (or destination) of the elevator
+	private Vector3 velocity;           // Velocity of the object when starting movement
+	private Vector3 targetPos;          // Position of the destination of the elevator
+	private bool targetSet;             // Flag set when both targets have been set from ElevatorCables.cs
+	private enum target                 // Enum of target types
+	{
+		Top,
+		Bottom
+	};
 
-    // Use this for initialization
-    void Awake () {
-        currentTarget = target.Bottom;
-        velocity = Vector3.zero;
+	// Use this for initialization
+	void Awake () {
+		currentTarget = target.Bottom;
+		velocity = Vector3.zero;
 		automatic = false;
-    }
-	
+	}
+
 	// Update is called once per frame
 	void Update () {
-        // If the targets have not been created and set yet, return. Otherwise we will get null errors
+		// If the targets have not been created and set yet, return. Otherwise we will get null errors
 		if (!targetSet || !automatic)
-        {
-            return;
-        }
+		{
+			return;
+		}
 
-        // Move towards the current target using SmoothDamp
-        switch (currentTarget)
-        {
-            case target.Top:
-                targetPos = CableTop;
-                transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, TravelTime);
-                break;
-            case target.Bottom:
-                targetPos = CableBotton;
-                transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, TravelTime);
-                break;
-            default:
-                targetPos = CableTop;
-                transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, TravelTime);
-                break;
-        }
+		// Move towards the current target using SmoothDamp
+		switch (currentTarget)
+		{
+		case target.Top:
+			targetPos = CableTop;
+			transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, TravelTime);
+			break;
+		case target.Bottom:
+			targetPos = CableBotton;
+			transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, TravelTime);
+			break;
+		default:
+			targetPos = CableTop;
+			transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPos, ref velocity, TravelTime);
+			break;
+		}
 
-        // If we are close enough to the current target, switch to the other one
-        if (Vector3.Distance(transform.localPosition, targetPos) < 0.00003f)
-        {
-            UpdateTarget();
-        }
-    }
+		// If we are close enough to the current target, switch to the other one
+		if (Vector3.Distance(transform.localPosition, targetPos) < 0.00003f)
+		{
+			UpdateTarget();
+		}
+	}
 
-    /// <summary>
-    /// Change target for the elevator to move towards
-    /// </summary>
-    public void UpdateTarget()
-    {
-        targetSet = true;
+	/// <summary>
+	/// Change target for the elevator to move towards
+	/// </summary>
+	public void UpdateTarget()
+	{
+		targetSet = true;
 
-        // Switch the target position
-        switch (currentTarget)
-        {
-            case target.Top:
-                currentTarget = target.Bottom;
-                break;
-            case target.Bottom:
-                currentTarget = target.Top;
-                break;
-            default:
-                currentTarget = target.Bottom;
-                break;
-        }
-    }
+		// Switch the target position
+		switch (currentTarget)
+		{
+		case target.Top:
+			currentTarget = target.Bottom;
+			break;
+		case target.Bottom:
+			currentTarget = target.Top;
+			break;
+		default:
+			currentTarget = target.Bottom;
+			break;
+		}
+	}
 
 	/// <summary>
 	/// Set destinations for the elevators and start them
