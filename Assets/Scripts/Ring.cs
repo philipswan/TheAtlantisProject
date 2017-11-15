e94755530b179509e6b6543cb9de8bc909205c23
Neil Orgon <neil.orgon@gmail.com>
2017-11-14 17:50:44 -0800

    Fix tram orientation and scale

--- Assets/Scripts/Ring.cs
+++ Assets/Scripts/Ring.cs
@@ -5,18 +5,21 @@
  * It was shortened by about 30 lines (and possibly sped up by a factor of 2) by consolidating math & loops and removing intermediate Collections.
  */
 using UnityEngine;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
 public class Ring : MonoBehaviour {
 
+	public GameObject Tram;
     public float TubeRadius = 0.001f;
-    public int numSegments = 100;
-    public int numTubes = 12;
+    public int NumSegments = 100;
+    public int NumTubes = 12;
 	[Tooltip("Set true if this script is attached to the tram ring object. This will place the ring lower.")]
 	public bool TramRing;
 
-    private float tetheredRingRadius;
-	private Constants.Configuration config;		// Holds reference to config file
+	private List<GameObject> trams = new List<GameObject>();	// Holds all trams in the scene
+    private float tetheredRingRadius;							// Calculated radius of ring
+	private Constants.Configuration config;						// Holds reference to config file
 
     void Start() {
 		config = Constants.Configuration.Instance;
@@ -37,7 +40,7 @@ public class Ring : MonoBehaviour {
 
     public void RefreshRing() {
         // Total vertices
-        int totalVertices = numSegments * numTubes;
+        int totalVertices = NumSegments * NumTubes;
 
         // Total primitives
         int totalPrimitives = totalVertices * 2;
@@ -53,29 +56,34 @@ public class Ring : MonoBehaviour {
         int[] triangleIndices = new int[totalIndices];
 
         // Calculate size of a segment and a tube
-        float segmentSize = 2 * Mathf.PI / (float)numSegments;
-        float tubeSize = 2 * Mathf.PI / (float)numTubes;
+        float segmentSize = 2 * Mathf.PI / (float)NumSegments;
+        float tubeSize = 2 * Mathf.PI / (float)NumTubes;
 
         // Create floats for our xyz coordinates
-        float x, y, z;
+        float x = 0, y = 0, z = 0;
+		float prevx = 0, prevy = 0, prevz = 0;
 
         // Begin loop that fills in both arrays
-        for (int i = 0; i < numSegments; i++)
+        for (int i = 0; i < NumSegments; i++)
         {
             // Find next (or first) segment offset
-            int n = (i + 1) % numSegments; // changed segmentList.Count to numSegments
+            int n = (i + 1) % NumSegments; // changed segmentList.Count to numSegments
 
             // Find the current and next segments
-            int currentTubeOffset = i * numTubes;
-            int nextTubeOffset = n * numTubes;
-            float normi = (float)i / numSegments * Mathf.PI;
+            int currentTubeOffset = i * NumTubes;
+            int nextTubeOffset = n * NumTubes;
+            float normi = (float)i / NumSegments * Mathf.PI;
             float funci = 0.5f - (Mathf.Cos(normi) / 2.0f);
             float angle = funci * 2 * Mathf.PI;
 
-            for (int j = 0; j < numTubes; j++)
+            for (int j = 0; j < NumTubes; j++)
             {
+				prevx = x;
+				prevy = y;
+				prevz = z;
+
                 // Find next (or first) vertex offset
-                int m = (j + 1) % numTubes; // changed currentTube.Count to numTubes
+                int m = (j + 1) % NumTubes; // changed currentTube.Count to numTubes
 
                 // Find the 4 vertices that make up a quad
                 int iv1 = currentTubeOffset + j;
@@ -89,9 +97,21 @@ public class Ring : MonoBehaviour {
                 z = (tetheredRingRadius + TubeRadius * Mathf.Cos(j * tubeSize)) * Mathf.Sin(angle);
                 y = TubeRadius * Mathf.Sin(j * tubeSize);
 
-				GameObject g = new GameObject("Ring position");
-				g.transform.parent = transform;
-				g.transform.localPosition = new Vector3 (x,y,z);
+				if (TramRing && i >= trams.Count)
+				{
+					GameObject tram = Instantiate(Tram, transform);
+					tram.transform.localPosition = new Vector3(x,y,z);
+					tram.name = "Tram: " + trams.Count.ToString();
+					tram.transform.localScale = new Vector3(6e-7f, 6e-7f, 6e-7f);
+					tram.transform.LookAt(transform.TransformPoint(new Vector3(prevx, prevy, prevz)));
+
+					Vector3 rot = Vector3.zero;
+					rot.z = -125;
+					tram.transform.GetChild(0).localRotation = Quaternion.Euler(rot);
+
+					tram.SetActive(true);
+					trams.Add(tram);
+				}
 
                 // Add the vertex to the vertex array
                 vertices[iv1] = new Vector3(x, y, z);
@@ -106,6 +126,7 @@ public class Ring : MonoBehaviour {
                 triangleIndices[iv1 * 6 + 5] = iv1;
             }
         }
+
         mesh.vertices = vertices;
         mesh.triangles = triangleIndices;
 
