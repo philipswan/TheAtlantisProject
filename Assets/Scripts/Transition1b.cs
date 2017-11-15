417772026c8fa4663dcbf645f028e83505743e19
philipswan <phil.swan@hotmail.com>
2017-11-14 21:41:13 -0800

    Wrote even tighter code for transitioning

--- Assets/Scripts/Transition1b.cs
+++ Assets/Scripts/Transition1b.cs
@@ -33,12 +33,38 @@ public class Transition1b : MonoBehaviour {
 		}
     }
 
-	/// <summary>
-	/// Move the system to the proper transforms in the Keys list
-	/// </summary>
-	/// <param name="scene">Scene.</param>
-	/// <param name="blend">Blend.</param>
-	private void UpdateSystem(int scene, float blend)
+    private int max(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+    private int min(int a, int b)
+    {
+        return (a < b) ? a : b;
+    }
+    /// <summary>
+    /// Move the system to the proper transforms in the Keys list
+    /// </summary>
+    /// <param name="scene">Scene.</param>
+    /// <param name="blend">Blend.</param>
+    private void UpdateSystem(int scene, float blend)
+    {
+        int index0 = max(0, min(Keys.Count - 1, (int)Mathf.Floor(scene / 2)));
+        int index1 = max(0, min(Keys.Count - 1, (int)Mathf.Floor((scene+1) / 2)));
+
+        transform.localPosition = Vector3.Lerp(Keys[index0].position, Keys[index1].position, Mathf.Pow(blend, 1f));
+        transform.localRotation = Quaternion.Lerp(Keys[index0].localRotation, Keys[index1].localRotation, Mathf.Pow(blend, 1.05f));
+        transform.localScale = Vector3.Lerp(Keys[index0].localScale, Keys[index1].localScale, Mathf.Pow(blend, 1.75f));
+    }
+    // 001122334455667788
+    //  001122334455667788
+
+    /*
+    /// <summary>
+    /// Move the system to the proper transforms in the Keys list
+    /// </summary>
+    /// <param name="scene">Scene.</param>
+    /// <param name="blend">Blend.</param>
+    private void UpdateSystem(int scene, float blend)
 	{
 		int index = (int)Mathf.Floor(scene/2);
 
@@ -55,6 +81,7 @@ public class Transition1b : MonoBehaviour {
 			transform.localScale = Vector3.Lerp (Keys[index].localScale, Keys[index+1].localScale, Mathf.Pow (blend, 1.75f));
 		}
 	}
+    */
 
 	/// <summary>
 	/// Set the transform for the user's elevator
